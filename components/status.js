5432d3fb41a5b3fa8d0b76d727b28849c8928c76
jwronquillo <qjwpronquillo01@tip.edu.ph>
2024-03-25 15:05:27 +0800

    Connection Check Function

--- components/status.js
+++ components/status.js
@@ -1,18 +1,30 @@
 import { Constants } from 'expo';
-import { NetInfo, Platform, StatusBar, StyleSheet, Text, View } from 'react-native-web';
+import { Platform, StatusBar, StyleSheet, Text, View } from 'react-native-web';
+import NetInfo from "@react-native-community/netinfo";
 import React from 'react';
 
 export default class Status extends React.Component{
     state = {
-        info: null,
+        info: 'none',
     };
+
+    componentDidMount() {
+        this.subcription = NetInfo.addEventListener(status => {
+            console.log('Network Status Changed', status);
+            this.setState({ info: status.type });
+        });
+    }
+
+    componentWillUnmount() {
+        if (this.subcription) {
+            this.subcription();
+        }
+    }
 
     render() {
         const {info} = this.state;
-
         const isConnected = info !== 'none';
         const backgroundColor = isConnected ? 'white' : 'red';
-
         const statusBar = (
             <StatusBar
             backgroundColor={backgroundColor}
@@ -20,11 +32,20 @@ export default class Status extends React.Component{
             animated={false}
             />  
         );
+        const messageContainer = (
+            <View style={styles.messageContainer} PointerEvents={'none'}>
+                {statusBar}
+                {!isConnected && (
+                <View style={styles.bubble}>
+                    <Text style={styles.text}>No network connection</Text>
+                </View>
+                )}
+            </View>
+            );
         if(Platform.OS == 'ios'){
             return <View style={[styles.status, {backgroundColor}]}></View>
         }
-
-        return null;
+        return messageContainer;
     }
 }
 
@@ -34,5 +55,23 @@ const styles = StyleSheet.create({
     status: {
         zIndex: 1,
         height: statusHeight,
+    },
+    messageContainer: {
+        zIndex: 1,
+        position: 'absolute',
+        top: statusHeight + 50,
+        right: 0,
+        left: 0,
+        height: 80,
+        alignItems: 'center',
+    },
+    bubble: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 20,
+        backgroundColor: 'red',
+    },
+    text: {
+        color: 'white',
     }
 })
